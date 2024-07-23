74ab72e3aa11b7f7ef5e685be98659ee9000a635
윤명기 <theyoonicon@gmail.com>
2024-07-23 17:49:44 +0900

    ver 0.1.2:config replacement

--- config/development.py
+++ config/development.py
@@ -1,9 +1,11 @@
-import os
+# 개발 환경을 담당할 파일
 
-BASE_DIR = os.path.dirname(__file__)
+from config.default import *
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'park.db'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+SECRET_KEY = "dev"
+
 
 # SQLALCHEMY_DATABASE_URI는 데이터베이스 접속 주소
 # SQLALCHEMY_TRACK_MODIFICATIONS는 SQLAlchemy의 이벤트를 처리하는 옵션
@@ -11,3 +13,4 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 # 앞으로 모델을 추가하거나 변경할 때는
 # flask db migrate 명령과 flask db upgrade 명령만 사용할 것이다.
 # 즉, 앞으로 데이터베이스 관리를 위해 여러분이 반드시 알아야 할 명령어는 다음 2가지이다.
+
--- park/__init__.py
+++ park/__init__.py
@@ -2,8 +2,7 @@ from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
-
-import config
+# import config
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -13,7 +12,8 @@ from . import models
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(config)
+    # app.config.from_object(config)
+    app.config.from_envvar('APP_CONFIG_FILE')
     
     # ORM
     db.init_app(app)
